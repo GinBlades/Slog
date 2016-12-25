5c93ecfc76e11b0e7c53ff7ca9533344d6c17741
Shaun Martin <webmaster@ginblades.com>
2016-12-25 11:54:24 -0600

    Starting user management.

---
+++ src/SlogWeb/Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using SlogWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using SlogWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using SlogWeb.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SlogWeb.Controllers {
+    [Authorize(Roles = "Administrators")]
+    public class UsersController : Controller {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context) {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index() {
+            var users = await _context.Users.Include(u => u.Roles).ToListAsync();
+            var roles = await _context.Roles.ToListAsync();
+            var userList = new List<UserViewModel>();
+            foreach (var user in users) {
+                userList.Add(new UserViewModel(user, roles, containsAllRoles: true));
+            }
+            return View(userList);
+        }
+
+        public async Task<IActionResult> Details(string id) {
+            if (string.IsNullOrWhiteSpace(id)) {
+                return NotFound();
+            }
+            var user = await _context.Users.Include(u => u.Roles).SingleOrDefaultAsync(u => u.Id == id);
+            if (user == null) {
+                return NotFound();
+            }
+            var roles = await _context.Roles.Where(r => user.Roles.Select(ur => ur.RoleId).Contains(r.Id)).ToListAsync();
+
+            return View(new UserViewModel(user, roles, containsAllRoles: false));
+        }
+
+        public IActionResult Create() {
+            return View(new ApplicationUser());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ApplicationUser user) {
+            if (ModelState.IsValid) {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Details", new { Id = user.Id });
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Edit(string id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Id == id);
+            if (user == null) {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, ApplicationUser user) {
+            if (ModelState.IsValid) {
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Details", new { Id = id });
+            }
+            return View(user);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id) {
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Id == id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/SlogWeb/ViewModels/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using SlogWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlogWeb.ViewModels {
+    public class UserViewModel {
+        public UserViewModel() { }
+        public UserViewModel(ApplicationUser user) {
+            Id = user.Id;
+            UserName = user.UserName;
+            Email = user.Email;
+        }
+
+        // Using this(user) calls constructor matching that signature
+        // 'containsAllRoles' flag to decide if passed in roles are specific to the user or
+        // all roles for the application. Defaults to true, all application roles.
+        public UserViewModel(ApplicationUser user, IEnumerable<IdentityRole> roles, bool containsAllRoles = true) : this(user) {
+            if (containsAllRoles) {
+                var userRoleIds = user.Roles.Select(r => r.RoleId);
+                var userRoles = roles.Where(r => userRoleIds.Contains(r.Id));
+                RoleNames = userRoles.Select(r => r.Name);
+            } else {
+                RoleNames = roles.Select(r => r.Name);
+            }
+        }
+
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> RoleNames { get; set; } = new List<string>();
+    }
+}
